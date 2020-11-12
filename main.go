53701bf32dfa10e4f462fa98eb2abcca60915d80
Gilgamesh-y <805655528@qq.com>
2020-11-12 11:53:50 +0800

    update

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 )
 
 type Tree struct {
-	RootNode TreeNode
+	RootNode *TreeNode
 }
 
 type TreeNode struct {
@@ -15,8 +15,6 @@ type TreeNode struct {
 	LeftNode *TreeNode
 	RightNode *TreeNode
 }
-
-var tree *Tree
 
 /**
               5
@@ -26,35 +24,45 @@ var tree *Tree
  */
 
 func main() {
+	tree := &Tree{}
 	arr := []int{5,3,1,7,9,2,4,6,8,10}
 	for _, val := range arr {
-		CreateTree(val)
+		tree.CreateTree(val)
 	}
 	println("先序遍历begin-------")
-	preTraverseBTree(tree.RootNode)
+	tree.RootNode.PreTraverseBTree()
 	println("先序遍历end-------")
 
 	println("中序遍历begin-------")
-	inTraverseBTree(tree.RootNode)
+	tree.RootNode.InTraverseBTree()
 	println("中序遍历end-------")
 
 	println("后序遍历begin-------")
-	nextTraverseBTree(tree.RootNode)
+	tree.RootNode.NextTraverseBTree()
 	println("后序遍历end-------")
 
-	println("树的深度-------"+strconv.Itoa(getHeight(tree.RootNode)))
+	println("树的深度-------"+strconv.Itoa(tree.getHeight(tree.RootNode)))
 
 	println("查找begin------")
-	str, _ := json.Marshal(Search(1, &tree.RootNode))
-	println(string(str))
+	pnode, node, _ := tree.Search(5, tree.RootNode, tree.RootNode, false)
+	pnodestr, _ := json.Marshal(pnode)
+	println("父节点"+string(pnodestr))
+	nodestr, _ := json.Marshal(node)
+	println("子节点"+string(nodestr))
 	println("查找end------")
+
+	println("删除begin-------")
+	tree.DelNode(3)
+	println("删除end-------")
+	nodestr, _ = json.Marshal(tree.RootNode)
+	println("删除后的树"+string(nodestr))
 }
 
-func CreateTree(value int) {
-	if tree == nil {
-		tree = &Tree{TreeNode{Value: value}}
+func (tree *Tree) CreateTree(value int) {
+	if tree.RootNode == nil {
+		tree.RootNode =&TreeNode{Value: value}
 	} else {
-		currentNode := &tree.RootNode
+		currentNode := tree.RootNode
 		for currentNode != nil {
 			if value > currentNode.Value {
 				if currentNode.RightNode == nil {
@@ -79,14 +87,14 @@ func CreateTree(value int) {
 /**
  * 先序遍历
  */
-func preTraverseBTree(node TreeNode) {
-	if node != (TreeNode{}) {
+func (node *TreeNode) PreTraverseBTree() {
+	if node != nil {
 		fmt.Println(node.Value)
 		if node.LeftNode != nil {
-			preTraverseBTree(*node.LeftNode)
+			node.LeftNode.PreTraverseBTree()
 		}
 		if node.RightNode != nil {
-			preTraverseBTree(*node.RightNode)
+			node.RightNode.PreTraverseBTree()
 		}
 	}
 }
@@ -94,14 +102,14 @@ func preTraverseBTree(node TreeNode) {
 /**
  * 中序遍历
  */
-func inTraverseBTree(node TreeNode) {
-	if node != (TreeNode{}) {
+func (node *TreeNode) InTraverseBTree() {
+	if node != nil {
 		if node.LeftNode != nil {
-			inTraverseBTree(*node.LeftNode)
+			node.LeftNode.InTraverseBTree()
 		}
 		fmt.Println(node.Value)
 		if node.RightNode != nil {
-			inTraverseBTree(*node.RightNode)
+			node.RightNode.InTraverseBTree()
 		}
 	}
 }
@@ -109,13 +117,13 @@ func inTraverseBTree(node TreeNode) {
 /**
  * 后序遍历
  */
-func nextTraverseBTree(node TreeNode) {
-	if node != (TreeNode{}) {
+func (node *TreeNode) NextTraverseBTree() {
+	if node != nil {
 		if node.LeftNode != nil {
-			nextTraverseBTree(*node.LeftNode)
+			node.LeftNode.NextTraverseBTree()
 		}
 		if node.RightNode != nil {
-			nextTraverseBTree(*node.RightNode)
+			node.RightNode.NextTraverseBTree()
 		}
 		fmt.Println(node.Value)
 	}
@@ -124,17 +132,17 @@ func nextTraverseBTree(node TreeNode) {
 /**
  * 获取树的高度
  */
-func getHeight(node TreeNode) int {
-	if node == (TreeNode{}) {
+func (tree *Tree) getHeight(node *TreeNode) int {
+	if node == nil {
 		return 0
 	}
 	leftHeight := 0
 	rightHeight := 0
 	if node.LeftNode != nil {
-		leftHeight = getHeight(*node.LeftNode)
+		leftHeight = tree.getHeight(node.LeftNode)
 	}
 	if node.RightNode != nil {
-		rightHeight = getHeight(*node.RightNode)
+		rightHeight = tree.getHeight(node.RightNode)
 	}
 	maxHeight := leftHeight
 	if rightHeight > maxHeight {
@@ -143,38 +151,83 @@ func getHeight(node TreeNode) int {
 	return maxHeight + 1
 }
 
-func Search(value int, node *TreeNode) *TreeNode {
+func (tree *Tree) Search(value int, parrentNode *TreeNode, node *TreeNode, isLeft bool) (*TreeNode, *TreeNode, bool) {
 	if node == nil {
-		return nil
+		return parrentNode, nil, isLeft
+	}
+	if parrentNode == nil {
+		return nil, nil, isLeft
 	}
 	if value < node.Value {
-		return Search(value, node.LeftNode)
+		isLeft = true
+		return tree.Search(value, node, node.LeftNode, isLeft)
 	}
 	if value > node.Value {
-		return Search(value, node.RightNode)
+		isLeft = false
+		return tree.Search(value, node, node.RightNode, isLeft)
 	}
-	return node
+
+	return parrentNode, node, isLeft
 }
 
-func GetChild(node *TreeNode) (*TreeNode,bool) {
-	if node.LeftNode != nil {
-		return node.LeftNode, true
+func (node *TreeNode) HasOneChild() (*TreeNode, bool) {
+	isLeft := false
+	if node.LeftNode != nil && node.RightNode == nil {
+		isLeft = true
+		return node.LeftNode, isLeft
+	}
+	if node.RightNode != nil && node.LeftNode == nil {
+		return node.RightNode, isLeft
+	}
+	return nil, isLeft
+}
+
+func (node *TreeNode) HasTwoChild() bool {
+	if node.LeftNode != nil && node.RightNode != nil {
+		return true
+	}
+	return false
+}
+
+func (node *TreeNode) GetLeftMaxNode(parrentNode *TreeNode,depth int) (*TreeNode, *TreeNode) {
+	if node.LeftNode == nil && depth == 0 {
+		return nil, node
 	}
 	if node.RightNode != nil {
-		return node.RightNode, true
+		return node.RightNode.GetLeftMaxNode(node, depth + 1)
 	}
-	return nil, false
+
+	return parrentNode, node
 }
 
-func delNode(value int) {
-	node := Search(value, &tree.RootNode)
-	childNode, hasChild := GetChild(node)
-	// 如果是叶子节点
-	if !hasChild {
-		node = nil
+func (tree *Tree) DelNode(value int) bool {
+	pnode, node, isLeft := tree.Search(value, tree.RootNode, tree.RootNode, false)
+	if node == nil {
+		return false
 	}
-	// 有左子节点或右子节点
+	// 有左子节点和右子节点
+	if node.HasTwoChild() {
+		lParrentMaxNode, lMaxNode := node.GetLeftMaxNode(node, 0)
+		node.Value = lMaxNode.Value
+		lParrentMaxNode.RightNode = nil
+		return true
+	}
+	childNode, _ := node.HasOneChild()
+	// 如果有子节点
 	if childNode != nil {
-
+		childNode = nil
+		return false
 	}
+	// 如果没有子节点
+	if childNode == nil {
+		if isLeft {
+			pnode.LeftNode = nil
+			return true
+		}
+		pnode.RightNode = nil
+		return true
+	}
+	// 只有左子节点或右子节点
+	*node = *childNode
+	return true
 }
